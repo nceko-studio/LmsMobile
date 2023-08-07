4577820da67ffd56b0a4f2c4c3919b81b230f688
nceko <nc.ekohidayat@gmail.com>
2023-08-08 00:31:32 +0800

    Persiapan Project

---
+++ app/src/main/java/com/example/lmspoltekgo/Kursus.java
@@ -0,0 +1,14 @@
+package com.example.lmspoltekgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kursus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kursus);
+    }
+}
---
+++ app/src/main/java/com/example/lmspoltekgo/Tugas.java
@@ -0,0 +1,14 @@
+package com.example.lmspoltekgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tugas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tugas);
+    }
+}
