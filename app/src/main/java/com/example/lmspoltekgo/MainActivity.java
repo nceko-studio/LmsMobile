67799f5d925d2ac9f74bb31ece024d94cc97ef3f
nceko <nc.ekohidayat@gmail.com>
2023-08-08 02:15:56 +0800

    SplashScreen FIX

--- app/src/main/java/com/example/lmspoltekgo/MainActivity.java
+++ app/src/main/java/com/example/lmspoltekgo/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.lmspoltekgo;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, Kursus.class));
+                finish();
+            }
+        },5000);
     }
 }
