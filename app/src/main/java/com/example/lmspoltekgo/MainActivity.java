4903c2e77cdf0bd20d7b9fce73c59e20272d2aa8
nceko <nc.ekohidayat@gmail.com>
2023-08-09 22:21:05 +0800

    Tambah Tampilan Login

--- app/src/main/java/com/example/lmspoltekgo/MainActivity.java
+++ app/src/main/java/com/example/lmspoltekgo/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(MainActivity.this, Kursus.class));
+                startActivity(new Intent(MainActivity.this, Login.class));
                 finish();
             }
         },5000);
